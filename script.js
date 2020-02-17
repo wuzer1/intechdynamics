2e79d45f2a12b3bd50252d82accd923aafd14e2f
coding507 <33985116+coding507@users.noreply.github.com>
2020-02-17 18:20:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 $(document).ready(function(){
     $(window).scroll(function() { // check if scroll event happened
         if ($(document).scrollTop() > 50) { // check if user scrolled more than 50 from top of the browser window
-            $("#primary-nav").css("background", "linear-gradient(-145deg, rgba(82,22,170,0.9) 0%, rgba(28,0,59,0.9) 72%)"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-            // $("#logo").attr('src','logo_mic.png');
+            $("#primary-nav").css("background", "linear-gradient(-145deg, rgba(82,22,170,0.9) 0%, rgba(28,0,59,0.9) 72%)"); // if yes, adding gradient color to navbar and changes logo
             $("#logo_mic").css('display','inline-block');
             $("#logo").css('display','none');
         } else {
-            $("#primary-nav").css("background", "transparent"); // if not, change it back to transparent
-            // $("#logo").attr('src','Intech_Logo.png');
+            $("#primary-nav").css("background", "transparent"); // // if not, change it back as it was before
             $("#logo_mic").css('display','none');
             $("#logo").css('display','inline-block');
         }
